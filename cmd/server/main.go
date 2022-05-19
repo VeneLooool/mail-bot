eace79e5b303f13c3db9b6f87e31b7ed34c90e71
VeneLooool <timofej.gromb38@gmail.com>
2022-05-19 17:33:58 +0300

    impimented rest api

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	api2 "gitlab.ozon.dev/VeneLooool/homework-2/api"
 	"gitlab.ozon.dev/VeneLooool/homework-2/internal/app/server"
 	"google.golang.org/grpc"
+	"log"
 	"net"
-	"time"
+	"net/http"
 )
+
+func runRest() {
+	/*conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = api2.RegisterMailServHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())*/
+	mux := runtime.NewServeMux()
+	err := api2.RegisterMailServHandlerFromEndpoint(context.Background(), mux, "localhost:8080", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := http.Server{
+		Handler: mux,
+	}
+	l, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = server.Serve(l)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	newServer := server.Server{}
@@ -22,11 +68,40 @@ func main() {
 	}
 	grpcServer := grpc.NewServer(opts...)
 	api2.RegisterMailServServer(grpcServer, &newServer)
-	err = grpcServer.Serve(listener)
-	if err != nil {
-		panic(err)
-	}
-	for {
-		time.Sleep(time.Millisecond * 100)
-	}
+	//err = grpcServer.Serve(listener)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for {
+	//	time.Sleep(time.Millisecond * 100)
+	//}
+	//go func() {
+	go runRest()
+	log.Fatalln(grpcServer.Serve(listener))
+	//}()
+	/*
+		conn, err := grpc.DialContext(
+			context.Background(),
+			"0.0.0.0:8080",
+			grpc.WithBlock(),
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		if err != nil {
+			log.Fatalln("Failed to dial server:", err)
+		}
+
+		gwmux := runtime.NewServeMux()
+		// Register Greeter
+		err = api2.RegisterMailServHandler(context.Background(), gwmux, conn)
+		if err != nil {
+			log.Fatalln("Failed to register gateway:", err)
+		}
+
+		gwServer := &http.Server{
+			Addr:    ":8090",
+			Handler: gwmux,
+		}
+
+		log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+		log.Fatalln(gwServer.ListenAndServe())*/
 }
--- go.mod
+++ go.mod
@@ -5,16 +5,35 @@ go 1.17
 require (
 	github.com/emersion/go-imap v1.2.1
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
+
+	github.com/cention-sany/utf7 v0.0.0-20170124080048-26cad61bd60a // indirect
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/gogs/chardet v0.0.0-20191104214054-4b6791f73a28 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/googleapis v0.0.0-20220518201257-904345281236 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
+	github.com/jhillyerd/enmime v0.9.4 // indirect
+	github.com/jhillyerd/go.enmime v0.6.1 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sloonz/go-qprintable v0.0.0-20210417175225-715103f9e6eb // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/genproto v0.0.0-20220317150908-0efb43f6373e // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
